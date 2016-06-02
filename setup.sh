06b4a4b364cd430da15328a5e83857867dd11728
yuyuType <yu_yu81@hotmail.com>
2016-06-02 17:31:14 +0900

    not remove .vimrc

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-if [ -e ~/.vimrc ] ; then
-  rm ~/.vimrc
-fi
-ln -s ~/.vim/vimrc ~/.vimrc
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/dein
 rm installer.sh
